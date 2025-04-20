acf8494e6b011e95e294f1b2a78fc048e58c9696
FxMorin <28154542+fxmorin@users.noreply.github.com>
2025-04-20 13:08:40 -0400

    Move pollster alerts to its own module

---
+++ src/alerts/PollsterAlerts.js
@@ -0,0 +1,64 @@
+import { getJsonFile, normalizePollingFirmName, writeJsonToFile } from "../utils.mjs";
+import axios from "axios";
+
+const newPollsterDiscordWebhook = process.env.NEW_POLLSTER_DISCORD_WEBHOOK;
+
+export function identifyUntaggedPollsters(context, pollsters, hasApiDir) {
+    const knownUntaggedPollsters = hasApiDir ?
+        getJsonFile(context.apiDir + "_data/v" + context.apiVersion + "/untagged-pollsters.json") : []
+    const knownPollsters = ["voting results", "market opinion research"];
+    // Populate known pollsters
+    for (let pollster of pollsters) {
+        knownPollsters.push(normalizePollingFirmName(pollster.name));
+        if ("alternatives" in pollster) {
+            for (let alternative of pollster["alternatives"]) {
+                knownPollsters.push(normalizePollingFirmName(alternative));
+            }
+        }
+    }
+    const newUntaggedList = [];
+    const newUntaggedPollsters = [];
+    // Check which pollsters are untagged
+    for (let pollster of context.pollsterIndex) {
+        if (!knownPollsters.includes(pollster)) {
+            // Add pollster
+            newUntaggedList.push(pollster);
+            if (!knownUntaggedPollsters.includes(pollster)) {
+                newUntaggedPollsters.push(pollster);
+                knownUntaggedPollsters.push(pollster);
+            }
+            console.log("Unknown pollster: " + pollster);
+        }
+    }
+    if (newUntaggedPollsters.length > 0) {
+        let description = "";
+        for (let pollster of newUntaggedPollsters) {
+            if (description !== "") {
+                description += "\n";
+            }
+            description += "• " + pollster
+        }
+        const params = {
+            username: "Auditor",
+            avatar_url: "https://pollsteraudit.ca/assets/favicon/PollsterAudit_Logo.png",
+            content: "<@&1363180448825606234>",
+            embeds: [
+                {
+                    "title": "New Pollster" + (newUntaggedPollsters.length > 1 ? "s" : ""),
+                    "color": 14022436,
+                    "description": description
+                }
+            ]
+        };
+        axios.post(newPollsterDiscordWebhook, params, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).catch(function (error) {
+            throw error;
+        });
+    }
+    if (newUntaggedList.length > 0) {
+        writeJsonToFile(context.mainDirectory, "untagged-pollsters", newUntaggedList);
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
-import axios from 'axios';
 import fs from 'node:fs';
-import { loadJson, writeJsonToFile, getJsonFile, normalizePollingFirmName } from './utils.mjs';
+import { loadJson, writeJsonToFile, getJsonFile } from './utils.mjs';
 import { WikipediaResolver } from "./sourceResolvers/WikipediaResolver.js";
 import { setupWebpages } from "./webpages/Webpages.js";
-
-const newPollsterDiscordWebhook = process.env.NEW_POLLSTER_DISCORD_WEBHOOK;
+import { identifyUntaggedPollsters } from "./alerts/PollsterAlerts.js";
 
 const parties = loadJson('../config/parties.json')["parties"];
 const pollsters = loadJson('../config/pollsters.json')["pollsters"];
@@ -14,69 +12,7 @@ const apiVersion = 1;
 const websiteDir = "./website/";
 const outputDir = "./output/";
 const apiDir = "./api/";
-const apiDataDir = apiDir + "_data/";
 const baseUrl = "https://api.pollsteraudit.ca/";
-
-function identifyUntaggedPollsters(directory, pollsterIndex, knownUntaggedPollsters) {
-    const knownPollsters = ["voting results", "market opinion research"];
-    // Populate known pollsters
-    for (let pollster of pollsters) {
-        knownPollsters.push(normalizePollingFirmName(pollster.name));
-        if ("alternatives" in pollster) {
-            for (let alternative of pollster["alternatives"]) {
-                knownPollsters.push(normalizePollingFirmName(alternative));
-            }
-        }
-    }
-    if (knownUntaggedPollsters === null) {
-        knownUntaggedPollsters = [];
-    }
-    const newUntaggedList = [];
-    const newUntaggedPollsters = [];
-    // Check which pollsters are untagged
-    for (let pollster of pollsterIndex) {
-        if (!knownPollsters.includes(pollster)) {
-            // Add pollster
-            newUntaggedList.push(pollster);
-            if (!knownUntaggedPollsters.includes(pollster)) {
-                newUntaggedPollsters.push(pollster);
-                knownUntaggedPollsters.push(pollster);
-            }
-            console.log("Unknown pollster: " + pollster);
-        }
-    }
-    if (newUntaggedPollsters.length > 0) {
-        let description = "";
-        for (let pollster of newUntaggedPollsters) {
-            if (description !== "") {
-                description += "\n";
-            }
-            description += "• " + pollster
-        }
-        const params = {
-            username: "Auditor",
-            avatar_url: "https://pollsteraudit.ca/assets/favicon/PollsterAudit_Logo.png",
-            content: "<@&1363180448825606234>",
-            embeds: [
-                {
-                    "title": "New Pollster" + (newUntaggedPollsters.length > 1 ? "s" : ""),
-                    "color": 14022436,
-                    "description": description
-                }
-            ]
-        };
-        axios.post(newPollsterDiscordWebhook, params, {
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        }).catch(function (error) {
-            throw error;
-        });
-    }
-    if (newUntaggedList.length > 0) {
-        writeJsonToFile(directory, "untagged-pollsters", newUntaggedList);
-    }
-}
 
 const index = async () => {
     console.log("Start indexing");
@@ -127,12 +63,8 @@ const index = async () => {
         // Setup webpages
         setupWebpages(context);
 
-        const untaggedPollstersPath = apiDataDir + "v" + apiVersion + "/untagged-pollsters.json";
-        identifyUntaggedPollsters(
-            mainDirectory,
-            pollsterIndex,
-            hasApiDir ? getJsonFile(untaggedPollstersPath) : null
-        );
+        // Find untagged pollsters
+        identifyUntaggedPollsters(context, pollsters, hasApiDir);
     } catch (error) {
         console.error('An error has occurred:', error);
         throw error;
